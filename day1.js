9807ee877026620910c6c27c7f9636fd084b7e6a
Dong Min <focuspdm4383@naver.com>
2024-10-13 13:39:54 +0900

    day1last

--- day1.js
+++ day1.js
@@ -1,18 +1,18 @@
 //console
 
-console.log('Hello word!');
+console.log("Hello word!");
 console.log(1);
 console.log(true);
-console.log({name:'홍길동',age:20,married:false});
+console.log({ name: "홍길동", age: 20, married: false });
 
 //콘솔은 소프트웨어 외적으로는 영향을 끼치지 않음 (개발자용 기능)
 
 //console 기능들
 
-console.log('로그 - 기본적인 출력'); //가장 많이 씀
-console.info('로그 - 기능적으로는 log와 같음. 사용하기에 따라 용도 구분 가능'); //브라우저 마다 달라서 쓰는곳도 있고 안쓰는곳도 있음
-console.warn('경고 - 문제가 될수있는 부분'); //문제가 발생할수 있음을 암시 시켜줌
-console.error('오류 - 에러 발생 상황'); // 문제가 발생했을때 알림
+console.log("로그 - 기본적인 출력"); //가장 많이 씀
+console.info("로그 - 기능적으로는 log와 같음. 사용하기에 따라 용도 구분 가능"); //브라우저 마다 달라서 쓰는곳도 있고 안쓰는곳도 있음
+console.warn("경고 - 문제가 될수있는 부분"); //문제가 발생할수 있음을 암시 시켜줌
+console.error("오류 - 에러 발생 상황"); // 문제가 발생했을때 알림
 
 //주석과 세미콜론
 
@@ -21,6 +21,34 @@ console.error('오류 - 에러 발생 상황'); // 문제가 발생했을때 알
 //한줄
 /*범위*/
 
-//세미콜론 
-//자바스크립트는 ; 생략가능 but 
-console.log('Hello');console.log('World') //다음과 같이 명령문을 한줄로 표시할때는 필수 
+//세미콜론
+//자바스크립트는 ; 생략가능 but
+console.log("Hello");
+console.log("World"); //다음과 같이 명령문을 한줄로 표시할때는 필수
+
+//변수와 상수
+
+console.log("Hello,", "철수");
+
+const SALUTATION = "Hello,";
+let person = "철수";
+
+console.log(SALUTATION, person);
+
+person = "영희";
+
+console.log(SALUTATION, person);
+
+let x;
+console.log(x); //undefined 값이 정해지지 않음
+//자바스크립트는 undefined 도 하나의 값임
+
+let y = 1;
+let z = y;
+console.log("변경전", x, y);
+y = "Hello!";
+
+//y의 값은 바뀌지만 z는 그대로 1 이다
+//자바스크립트에서는 C언어와 달리 메모리상 가리키는 위치가 변경되는것이다.
+
+//변수명 한글은 가능한데 잘 안쓴다.
