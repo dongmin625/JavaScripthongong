c807f4bf62eda136c83ffb858e183656bc055020
Dong Min <focuspdm4383@naver.com>
2024-10-14 13:53:22 +0900

    문자열 자료형 끝

--- day2_1.js
+++ day2_1.js
@@ -47,3 +47,52 @@ ${married ? "기혼" : "미혼"}입니다.`
 
 //다른 자료형도 사용할 수 있음
 console.log(`일더하기 이는 ${1 + 2}이다.`);
+
+x == y; //값이 같다.
+x === y; // 자료형도 값도 같다.
+x != y; //값이 다르다.
+x !== y; //값또는 자료형이 다르다.
+
+//==,!= 자료형을 구분하지 않음
+//===,!==을 지향함
+
+// ⚠️ 숫자 문자열 관련 주의!
+console.log(
+  100 > 12, // 숫자는 그 자체로 비교 true
+  '100' > '12', // 문자는 사전순으로 비교 false
+  '100' > 12, // 문자와 숫자를 비교하면 문자를 숫자로 변환 true
+);
+
+//부수효과
+
+let str1 = '헬로';
+str1 += ' 월드';
+
+// 부수효과
+console.log(str1);
+// 값 반환
+let str2 = str1 += '~~~';
+
+console.log(str2); //헬로 월드 ~~~
+
+// 부수효과
+console.log(str1); //헬로 월드 ~~~
+
+// ⚠️ 오류. 왼쪽 값은 부수효과의 대상(변수)이어야 함
+let str = '헬로' += '월드'; 
+
+// ⚠️ 오류. 왼쪽 값은 부수효과의 대상(변수)이어야 함
+const STR = '안녕~';
+STR += ' 반가워요!';
+
+//다른 자료형과 더하면 문자열(String) 반환
+let result = '안녕' + 1 + true;
+
+console.log(result);
+console.log(typeof result);
+
+result += null;//Object 자료형
+result += undefined;//undefined 자료형
+
+console.log(result);
+console.log(typeof result);//String
